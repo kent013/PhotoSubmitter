2c7356edfeedc7e5e35fce09c1c23500bc139dac
ISHITOYA Kentaro <ishitoya@rio.ne.jp>
2012-03-06 17:00:54 +0900

    Added initial codes derivered from http://github.com/kent013/tottepost

---
+++ UtilityCategories/UIImage+EXIF.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+GeoTagging.h
+//  tottepost
+//
+//  Created by ISHITOYA Kentaro on 11/12/25.
+//  Copyright (c) 2011 cocotomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface UIImage (EXIF)
+-(NSData *) geoTaggedDataWithLocation:(CLLocation *)location andComment:(NSString *)comment;
++(NSData *) geoTaggedData:(NSData *)data withLocation:(CLLocation *)location andComment:(NSString *)comment;
++(NSDictionary *)extractMetadata:(NSData *)data;
+@end
