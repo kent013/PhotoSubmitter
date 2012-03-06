d3b45444b50291afcd758e0e7d9af9bcb4a17984
ISHITOYA Kentaro <ishitoya@rio.ne.jp>
2012-03-07 02:14:47 +0900

    Added service implementations.

---
+++ Services/MixiPhotoSubmitter/MixiPhotoSubmitter.h
@@ -0,0 +1,14 @@
+//
+//  MixiPhotoSubmitter.h
+//  tottepost
+//
+//  Created by Ken Watanabe on 12/02/12.
+//  Copyright (c) 2012 cocotomo. All rights reserved.
+//
+
+#import "PhotoSubmitter.h"
+#import "MixiSDK.h"
+@interface MixiPhotoSubmitter : PhotoSubmitter<PhotoSubmitterInstanceProtocol,MixiDelegate, MixiSDKAuthorizerDelegate>{
+    __strong Mixi *mixi_;
+}
+@end
