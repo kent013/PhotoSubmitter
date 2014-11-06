3c44c5b94ed62da8af0718a43d3a4b41a45b385a
kent013 <kentaro.ishitoya@gmail.com>
2014-11-06 18:17:20 +0900

    [WIP] Adapting project to cocoapods

--- Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -6,11 +6,59 @@
 // project.
 
 
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 4
+
+// Base64nl
+#define COCOAPODS_POD_AVAILABLE_Base64nl
+#define COCOAPODS_VERSION_MAJOR_Base64nl 1
+#define COCOAPODS_VERSION_MINOR_Base64nl 2
+#define COCOAPODS_VERSION_PATCH_Base64nl 0
+
 // Expecta
 #define COCOAPODS_POD_AVAILABLE_Expecta
 #define COCOAPODS_VERSION_MAJOR_Expecta 0
 #define COCOAPODS_VERSION_MINOR_Expecta 3
 #define COCOAPODS_VERSION_PATCH_Expecta 1
+
+// FBNetworkReachability
+#define COCOAPODS_POD_AVAILABLE_FBNetworkReachability
+#define COCOAPODS_VERSION_MAJOR_FBNetworkReachability 1
+#define COCOAPODS_VERSION_MINOR_FBNetworkReachability 0
+#define COCOAPODS_VERSION_PATCH_FBNetworkReachability 1
+
+// ISO8601DateFormatterValueTransformer
+#define COCOAPODS_POD_AVAILABLE_ISO8601DateFormatterValueTransformer
+#define COCOAPODS_VERSION_MAJOR_ISO8601DateFormatterValueTransformer 0
+#define COCOAPODS_VERSION_MINOR_ISO8601DateFormatterValueTransformer 6
+#define COCOAPODS_VERSION_PATCH_ISO8601DateFormatterValueTransformer 0
+
+// KissXML
+#define COCOAPODS_POD_AVAILABLE_KissXML
+#define COCOAPODS_VERSION_MAJOR_KissXML 5
+#define COCOAPODS_VERSION_MINOR_KissXML 0
+#define COCOAPODS_VERSION_PATCH_KissXML 0
+
+// MAConfirmButton
+#define COCOAPODS_POD_AVAILABLE_MAConfirmButton
+#define COCOAPODS_VERSION_MAJOR_MAConfirmButton 1
+#define COCOAPODS_VERSION_MINOR_MAConfirmButton 5
+#define COCOAPODS_VERSION_PATCH_MAConfirmButton 0
+
+// NYXImagesKit
+#define COCOAPODS_POD_AVAILABLE_NYXImagesKit
+#define COCOAPODS_VERSION_MAJOR_NYXImagesKit 2
+#define COCOAPODS_VERSION_MINOR_NYXImagesKit 3
+#define COCOAPODS_VERSION_PATCH_NYXImagesKit 0
+
+// PDKeychainBindingsController
+#define COCOAPODS_POD_AVAILABLE_PDKeychainBindingsController
+#define COCOAPODS_VERSION_MAJOR_PDKeychainBindingsController 0
+#define COCOAPODS_VERSION_MINOR_PDKeychainBindingsController 0
+#define COCOAPODS_VERSION_PATCH_PDKeychainBindingsController 1
 
 // PhotoSubmitter
 #define COCOAPODS_POD_AVAILABLE_PhotoSubmitter
@@ -18,9 +66,99 @@
 #define COCOAPODS_VERSION_MINOR_PhotoSubmitter 1
 #define COCOAPODS_VERSION_PATCH_PhotoSubmitter 0
 
+// RKValueTransformers
+#define COCOAPODS_POD_AVAILABLE_RKValueTransformers
+#define COCOAPODS_VERSION_MAJOR_RKValueTransformers 1
+#define COCOAPODS_VERSION_MINOR_RKValueTransformers 1
+#define COCOAPODS_VERSION_PATCH_RKValueTransformers 0
+
+// Reachability
+#define COCOAPODS_POD_AVAILABLE_Reachability
+#define COCOAPODS_VERSION_MAJOR_Reachability 3
+#define COCOAPODS_VERSION_MINOR_Reachability 1
+#define COCOAPODS_VERSION_PATCH_Reachability 1
+
+// RegexKitLite
+#define COCOAPODS_POD_AVAILABLE_RegexKitLite
+#define COCOAPODS_VERSION_MAJOR_RegexKitLite 4
+#define COCOAPODS_VERSION_MINOR_RegexKitLite 0
+#define COCOAPODS_VERSION_PATCH_RegexKitLite 0
+
+// RestKit
+#define COCOAPODS_POD_AVAILABLE_RestKit
+#define COCOAPODS_VERSION_MAJOR_RestKit 0
+#define COCOAPODS_VERSION_MINOR_RestKit 23
+#define COCOAPODS_VERSION_PATCH_RestKit 3
+
+// RestKit/Core
+#define COCOAPODS_POD_AVAILABLE_RestKit_Core
+#define COCOAPODS_VERSION_MAJOR_RestKit_Core 0
+#define COCOAPODS_VERSION_MINOR_RestKit_Core 23
+#define COCOAPODS_VERSION_PATCH_RestKit_Core 3
+
+// RestKit/CoreData
+#define COCOAPODS_POD_AVAILABLE_RestKit_CoreData
+#define COCOAPODS_VERSION_MAJOR_RestKit_CoreData 0
+#define COCOAPODS_VERSION_MINOR_RestKit_CoreData 23
+#define COCOAPODS_VERSION_PATCH_RestKit_CoreData 3
+
+// RestKit/Network
+#define COCOAPODS_POD_AVAILABLE_RestKit_Network
+#define COCOAPODS_VERSION_MAJOR_RestKit_Network 0
+#define COCOAPODS_VERSION_MINOR_RestKit_Network 23
+#define COCOAPODS_VERSION_PATCH_RestKit_Network 3
+
+// RestKit/ObjectMapping
+#define COCOAPODS_POD_AVAILABLE_RestKit_ObjectMapping
+#define COCOAPODS_VERSION_MAJOR_RestKit_ObjectMapping 0
+#define COCOAPODS_VERSION_MINOR_RestKit_ObjectMapping 23
+#define COCOAPODS_VERSION_PATCH_RestKit_ObjectMapping 3
+
+// RestKit/Support
+#define COCOAPODS_POD_AVAILABLE_RestKit_Support
+#define COCOAPODS_VERSION_MAJOR_RestKit_Support 0
+#define COCOAPODS_VERSION_MINOR_RestKit_Support 23
+#define COCOAPODS_VERSION_PATCH_RestKit_Support 3
+
+// SBJson
+#define COCOAPODS_POD_AVAILABLE_SBJson
+#define COCOAPODS_VERSION_MAJOR_SBJson 4
+#define COCOAPODS_VERSION_MINOR_SBJson 0
+#define COCOAPODS_VERSION_PATCH_SBJson 1
+
+// SOCKit
+#define COCOAPODS_POD_AVAILABLE_SOCKit
+#define COCOAPODS_VERSION_MAJOR_SOCKit 1
+#define COCOAPODS_VERSION_MINOR_SOCKit 1
+#define COCOAPODS_VERSION_PATCH_SOCKit 0
+
+// SVProgressHUD
+#define COCOAPODS_POD_AVAILABLE_SVProgressHUD
+#define COCOAPODS_VERSION_MAJOR_SVProgressHUD 1
+#define COCOAPODS_VERSION_MINOR_SVProgressHUD 0
+#define COCOAPODS_VERSION_PATCH_SVProgressHUD 0
+
 // Specta
 #define COCOAPODS_POD_AVAILABLE_Specta
 #define COCOAPODS_VERSION_MAJOR_Specta 0
 #define COCOAPODS_VERSION_MINOR_Specta 2
 #define COCOAPODS_VERSION_PATCH_Specta 1
+
+// TransitionKit
+#define COCOAPODS_POD_AVAILABLE_TransitionKit
+#define COCOAPODS_VERSION_MAJOR_TransitionKit 2
+#define COCOAPODS_VERSION_MINOR_TransitionKit 1
+#define COCOAPODS_VERSION_PATCH_TransitionKit 0
+
+// UIImage-Categories
+#define COCOAPODS_POD_AVAILABLE_UIImage_Categories
+#define COCOAPODS_VERSION_MAJOR_UIImage_Categories 0
+#define COCOAPODS_VERSION_MINOR_UIImage_Categories 0
+#define COCOAPODS_VERSION_PATCH_UIImage_Categories 1
+
+// ZipArchive
+#define COCOAPODS_POD_AVAILABLE_ZipArchive
+#define COCOAPODS_VERSION_MAJOR_ZipArchive 1
+#define COCOAPODS_VERSION_MINOR_ZipArchive 3
+#define COCOAPODS_VERSION_PATCH_ZipArchive 0
 
--- PhotoSubmitter.podspec
+++ PhotoSubmitter.podspec
@@ -22,9 +22,23 @@ Pod::Spec.new do |s|
   s.author           = { "kent013" => "kentaro.ishitoya@gmail.com" }
   s.source           = { :git => "https://github.com/<GITHUB_USERNAME>/PhotoSubmitter.git", :tag => s.version.to_s }
   s.platform     = :ios, '7.0'
+  s.ios.deployment_target = '6.0'
   s.requires_arc = true
   s.source_files = 'Pod/Classes/*.{h,m}', 'Pod/Classes/{Libraries,Entities,UtilityCategories,Settings}/**/*.{h,m}'
   s.resource_bundles = {
     'PhotoSubmitter' => ['Pod/Assets/*.png']
   }
+  s.dependency "FBNetworkReachability"
+  s.dependency "KissXML"
+  s.dependency "RestKit"
+  s.dependency "SVProgressHUD"
+  s.dependency "MAConfirmButton"
+  s.dependency "UIImage-Categories"
+  s.dependency "NYXImagesKit"
+  s.dependency "ZipArchive"
+  s.dependency "PDKeychainBindingsController"
+  s.dependency "Reachability"
+  s.dependency "SBJson"
+  s.dependency "RegexKitLite"
+  s.dependency "Base64nl"
 end
